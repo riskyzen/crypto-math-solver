73614c0b907281105e2abf444bc25a2c76356bbf
aku <aku@unix64-pc.unix64-pc.com>
2020-06-16 10:17:42 +0700

    initial

---
+++ cryptomathsolver.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"CRYPTO MATH SOLVER"<<endl;
+    cout<<"1. Rsa"<<endl;
+    cout<<"2. elgamal"<<endl;
+    cout<<"3. Curva Eliptik"<<endl;
+    cout<<"4. Modular Exponen"<<endl;
+    cout<<"5. Modular Inverse"<<endl;
+    cout<<"6. Prime Check"<<endl;
+}
